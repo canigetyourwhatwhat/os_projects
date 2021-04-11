760546c9697a32d56c86d34adb89d5c226d2971e
canigetyourwhatwhat <daichiando98@gmail.com>
2021-04-11 15:22:10 +0900

    revised version

--- test.c
+++ test.c
@@ -10,14 +10,17 @@ void readInput(char *commands[])
     const char s[1] = " ";
     
     int length = read(STDIN_FILENO, args, 80);
-    commands[0] = strtok(args, s);
-    int i = 0;
-    while( commands[i] != NULL)
+    char *temp = strtok(args, s);
+    //int *commandLength = 0;
+    int i=0;
+    while( temp != NULL)
     {
         //printf(" %s\n", commands[i]);
         i++;
-        commands[i] = strtok(NULL, s);
-    }    
+        commands[i-1] = strdup(temp);
+        temp = strtok(NULL, s);
+    }
+    commands[i] = NULL;
 }
 
 int main(void)
@@ -35,10 +38,9 @@ int main(void)
     int i = 0;
     while(input[i] != NULL)
     {
-        printf(" %s\n", input[i]);
+        printf("%s\n", input[i]);
         i++;
     }
-    
 
     // Make a child by fork()
     pid_t pid = fork();
@@ -48,10 +50,20 @@ int main(void)
         printf("it is in child now \n");
         //int execret = execlp("/bin/ls", "ls", "-A", NULL);
         //char* concat = "/bin/";
+        
         int execret = execvp(input[0], input);
+        /*
+        char *cmd = "ls";
+        char *argv[3];
+        argv[0] = "ls";
+        argv[1] = "-al";
+        argv[2] = NULL;
+        execvp(cmd, argv);
+        */
+        
         //char* concat = strcat("/bin/", input[0]);
         //int execret = execvp(concat, input);
-        assert(execret >= 0);
+        //assert(execret >= 0);
         return 0;
     }
     else
